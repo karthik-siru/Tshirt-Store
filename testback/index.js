683c067e77b38c2aa1d62b1a035174a01f752eca
karthik-siru <karthiksiruvalam@gmail.com>
2021-08-13 21:53:21 +0530

    using middle wares for routes

--- testback/index.js
+++ testback/index.js
@@ -2,16 +2,36 @@ const express = require("express");
 
 const app = express();
 
-const port = 8001 ;
+const port = 8001;
 
-app.get("/"  , (req , res )=> {
-    return  res.send("hello mowa ") ;
+app.get("/", (req, res) => {
+  return res.send("hello mowa ");
 });
 
-app.get("/login"  , (req , res )=> {
-    return  res.send("This is a login page  ") ;
+// admin request using middle--ware
+
+//middleware
+
+const isLogged = (req, res,next) => {
+  console.log("yes logged in");
+  next();
+};
+
+const isAdmin = (req, res, next) => {
+  console.log("Admin check ");
+  next();
+};
+
+const admin = (req, res ) => {
+  return res.send("Welcome Admin");
+};
+
+app.get("/admin", isLogged, isAdmin, admin);
+
+app.get("/login", (req, res) => {
+  return res.send("This is a login page  ");
 });
 
-app.listen(port , () => {
-    console.log("server is up and running  ... ");
+app.listen(port, () => {
+  console.log("server is up and running  ... ");
 });
