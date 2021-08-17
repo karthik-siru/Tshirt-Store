b8e37201c49809f34015ced0ea248c9a8579e7aa
karthik-siru <karthiksiruvalam@gmail.com>
2021-08-17 21:49:40 +0530

    signin route created

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,20 +1,30 @@
-const { check, validationReq  } = require("express-validator");
+const { check, validationReq } = require("express-validator");
 const express = require("express");
 const router = express.Router();
 
-const { signup, signout } = require("../controllers/auth");
+const { signup, signout, signin } = require("../controllers/auth");
 
 router.post(
   "/signup",
   [
     // validators
-    check("name","name should be at least 5 chars long" ).isLength({ min: 3 }),
+    check("name", "name should be at least 5 chars long").isLength({ min: 3 }),
     check("email", "Please provide a valid email ").isEmail(),
-    check("password", "password mari ila pettav entra ").isLength({ min: 8 })
-
+    check("password", "password mari ila pettav entra ").isLength({ min: 8 }),
   ],
   signup
 );
+
+router.post(
+  "/signin",
+  [
+    // validators
+    check("email", "Please provide a valid email ").isEmail(),
+    check("password", "Please check your password").isLength({ min: 8 }),
+  ],
+  signin
+);
+
 // throw a request
 router.get("/signout", signout);
 module.exports = router;
