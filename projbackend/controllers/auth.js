ce40b2335992da797574b14eae035bdb1f61749f
karthik-siru <karthiksiruvalam@gmail.com>
2021-08-18 21:59:45 +0530

    signin authentication and testing using postman

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,13 +1,15 @@
 const User = require("../models/user");
 const { check, validationResult } = require("express-validator");
-
+const user = require("../models/user");
+var jwt = require("jsonwebtoken");
+var expressJwt = require("express-jwt");
 exports.signup = (req, res) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     return res.status(400).json({
-      message : errors.array()[0].msg,
-      param : errors.array()[0].param
+      message: errors.array()[0].msg,
+      param: errors.array()[0].param,
     });
   }
 
@@ -23,6 +25,40 @@ exports.signup = (req, res) => {
       email: user.email,
       id: user._id,
     });
+  });
+};
+
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+  // if there are errors... return back
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      message: errors.array()[0].msg,
+      param: errors.array()[0].param,
+    });
+  }
+
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User doesnot , exists ",
+      });
+    }
+
+    if ( !user.authenthicate(password)) {
+      return res.status(401).json({
+        error: "Email and Password doesn't  match ",
+      });
+    }
+    // create token
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+    // put the token in cookie
+    res.cookie("token", token, { expire: new Date() + 99999 });
+
+    //send response to the frontend
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, name, email, role } });
   });
 };
 
