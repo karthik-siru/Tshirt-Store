b76e4c89b4f9344c06aceccd51f279e96d42d36f
karthik-siru <karthiksiruvalam@gmail.com>
2021-09-24 21:35:59 +0530

    signin and signup setup and navi-bar

--- projfrontend/src/core/Base.js
+++ projfrontend/src/core/Base.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Menu from "./Menu";
 const Base = ({
   title = "My Title",
   description = "My desription",
@@ -7,6 +7,7 @@ const Base = ({
   children,
 }) => (
   <div>
+    <Menu />
     <div className="container-fluid">
       <div className="jumbotron bg-dark text-white text-center">
         <h2 className="display-4">{title}</h2>
--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -3,7 +3,11 @@ import { API } from "../backend";
 import Base from "./Base";
 function Home() {
   console.log("API IS", API);
-  return <Base title="Home Page" description="All about tshirts"></Base>;
+  return (
+    <Base title="Home Page" description="All about tshirts">
+      <h1>Welcome Bro</h1>
+    </Base>
+  );
 }
 
 export default Home;
